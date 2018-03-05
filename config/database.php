8efc301b20c0949646fe46b052672bf2b09837de
Mark Munsayac <munsayacmlj@gmail.com>
2018-03-05 21:13:21 +0800

    update database

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'sql12.freesqldatabase.com'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'sql12224763'),
+            'username' => env('DB_USERNAME', 'sql12224763'),
+            'password' => env('DB_PASSWORD', 'Bm57Yy6rZI'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
