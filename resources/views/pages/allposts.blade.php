c2522a63331a37b965918b4ab41ad7820a9e95ec
markibinex <mark.mu@ibinex.tech>
2018-08-31 00:42:18 +0800

    successfully implemented custom masonry style

--- resources/views/pages/allposts.blade.php
+++ resources/views/pages/allposts.blade.php
@@ -29,9 +29,8 @@
       </div>
     </nav>
         {!! $posts->appends($_GET)->links() !!}
-    <div class="grid pt-4 mt-3">
+  <!-- <div class="grid pt-4 mt-3">
 			@foreach ($posts as $post)
-			{{-- <div class="outer-card mr-3 mb-3" id="card_{{ $post->id }}" style="width: 300px;"> --}}
 				<div class="card bg-light mb-3 grid-sizer grid-item" id="card_{{ $post->id }}">
           <div class="card-header clearfix">
   					<a href="/people/{{ str_replace(" ", "-", strtolower($post->user->name)) }}" class="float-left">
@@ -90,7 +89,67 @@
 				</div>
 			{{-- </div> --}}
 			@endforeach
-		</div>
+		</div> --> {{-- grid --}}
+      <div class="custom-masonry">
+        @foreach ($posts as $post)
+          <div class="card custom-masonry__item bg-light mb-3" id="card_{{ $post->id }}">
+            <div class="card-header clearfix">
+              <a href="/people/{{ str_replace(" ", "-", strtolower($post->user->name)) }}" class="float-left">
+                <img src="{!! $post->user->getPhotoPath() !!}" class="user-pic">          
+                <span class="card-poster mt-1">
+                    {{ $post->user->name }}
+                </span>
+              </a>        
+              <span class="float-right action-btns">
+                @auth    
+                  @if ($post->user->name == Auth::user()->name)
+                    <a href='{{ url("posts/$post->id/edit") }}' class="edit-post p-1">
+                      <i class="far fa-edit"></i>
+                    </a>
+                    <a href="#" class="delete-post p-1" data-id="{{ $post->id }}">
+                      <i class="far fa-trash-alt"></i>
+                    </a>
+                  @else
+                    @if(in_array($post->user->id, $following))
+                    <button type="button" class="btn btn-warning btn-sm unfollow-user-btn uf-user-{{ $post->user->id }}" data-user="{{ $post->user->name }}" data-id="{{ $post->user->id }}" id="uf-btn-{{ $post->user->id }}"><i class="fas fa-user-times"></i></button>
+                    @else
+                    <button type="button" class="btn btn-info btn-sm follow-user-btn f-user-{{ $post->user->id }}" data-user="{{ $post->user->name }}" data-id="{{ $post->user->id }}" id="f-btn-{{ $post->user->id }}"><i class="fas fa-user-plus"></i></button>
+                    @endif
+                  @endif
+                @endauth
+              </span>
+            </div>
+                      
+            <div class="card-body">
+              <div class="card-title h5 pb-1">
+                  <a href='{{ url("posts/$post->id") }}'>{{ $post->title }}</a>
+              </div>
+              <p>{{ $post->content }}</p>
+            </div>
+            @if ($post->hasImage())
+              <a href='{{ url("posts/$post->id") }}'>
+                <img src='{!! $post->getImagepath() !!}'>
+              </a>
+            @endif
+            <div class="card-footer">
+              <div class="m-0">
+                <div class="num-comments">
+                  <a href="/posts/{{ $post->id }}#comment-body-{{ $post->id }}"><i class="fas fa-comments"></i> {{ $post->comments->count() }}</a>
+                </div>
+                <div>
+                  <i class="fas fa-tag"></i>
+                  Posted in
+                  <a href="/posts/?topic={{ strtolower($post->topic->topic) }}">{{ $post->topic->topic }}</a>
+                </div>
+                <div>
+                    <i class="far fa-clock"></i>
+                    {{ $post->created_at->diffForHumans() }}
+                </div>
+              </div>
+            </div>    
+          </div>
+        @endforeach
+      </div>
 	</div>
 @include('partials.modal')
 @endsection
